69a2ec918da4273aae274487e5a0f6e24cee24df
Gabriel Canosa <g.canosa90@gmail.com>
2024-01-04 11:24:11 +0100

    Add project files.

---
+++ IdentityDemoProject/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityDemoProject.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+
+    }
+}
