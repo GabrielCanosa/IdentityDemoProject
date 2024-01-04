c487495b7b941c0d967cb2e9a09270cd356c4f61
Gabriel Canosa <g.canosa90@gmail.com>
2024-01-04 12:46:07 +0100

    customize identity user table

---
+++ IdentityDemoProject/Models/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace IdentityDemoProject.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string Name { get; set; }
+        public string Lastname { get; set; }
+    }
+}
